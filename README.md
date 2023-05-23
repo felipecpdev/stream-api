8ed801d652a81ef4918e8320757a4c420f10241d
Felipe Contreras <felipe.contreras.dev@gmail.com>
2023-05-23 15:10:32 -0400

    distinct

--- README.md
+++ README.md
@@ -5,12 +5,28 @@
 ### Count
 
 ``` Java
-//       long count = Arrays.stream(colors).count();
-// long count = Arrays.asList(colors).stream().count();
-        long count = Stream.of(colors).count();
-         System.out.println(count);
+//long count = Arrays.stream(colors).count();
+//long count = Arrays.asList(colors).stream().count();
+  long count = Stream.of(colors).count();
+  System.out.println(count);
 ```
-### Count
 
+### Distinct
+
+``` Java
+//Distinct()
+long count = Arrays.asList(colors).stream()
+.distinct()
+.count();
+System.out.println(count);
+
+        Arrays.asList(colors).stream()
+                .distinct()
+                .forEach(System.out::println);
+        System.out.println();
+        Stream.of(colors)
+                .distinct()
+                .forEach(System.out::println);
+```
 
 
--- src/MainClass.java
+++ src/MainClass.java
@@ -2,12 +2,23 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class MainClass {
-    private static final String[] colors = {"Red", "Orange", "Green", "Blue", "Violet"};
+    private static final String[] colors = {"Red", "Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"};
 
     public static void main(String[] args) {
-//        long count = Arrays.stream(colors).count();
-//        long count = Arrays.asList(colors).stream().count();
-        long count = Stream.of(colors).count();
+        //Distinct()
+        long count = Arrays.asList(colors).stream()
+                .distinct()
+                .count();
         System.out.println(count);
+
+        Arrays.asList(colors).stream()
+                .distinct()
+                .forEach(System.out::println);
+        System.out.println();
+        Stream.of(colors)
+                .distinct()
+                .forEach(System.out::println);
+
+
     }
 }

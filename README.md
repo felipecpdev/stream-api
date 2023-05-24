673a879e3ce5630345ac5d801d6fe02a8331bd13
Felipe Contreras <felipe.contreras.dev@gmail.com>
2023-05-24 15:44:11 -0400

    reduce

--- README.md
+++ README.md
@@ -377,3 +377,43 @@ flatMap(user->user.getColors.stream().sorted().limit(2))
                 .map(User::getFirstname)
                 .forEach(System.out::println);
 ```
+
+## BinaryOperator
+
+### reduce
+
+```java
+//reduce - example 1
+        Integer reduce = Arrays.asList(1, 2, 3, 4, 5).stream()
+                .filter(integer -> integer > 2)
+                .reduce(0, (subTotal, element) -> subTotal + element);
+        System.out.println(reduce);
+
+        // example 2
+        Integer reduce1 = Arrays.asList(
+                        new User("Tony", 45, Arrays.asList("Red", "Green")),
+                        new User("Mark", 21, Arrays.asList("Blue", "Green", "White")),
+                        new User("Ben", 19, Arrays.asList("Green", "Violet", "Purple", "Grey")),
+                        new User("Claire", 37, Arrays.asList("White", "Black")),
+                        new User("Sarah", 49, Arrays.asList("Red", "Green", "Blue"))
+                ).stream()
+                .filter(user -> user.getAge() > 24)
+                .reduce(0, (subTotal, element) -> subTotal + element.getAge(), Integer::sum);
+
+        System.out.println(reduce1);
+
+        //example 3
+        int reduce2 = Arrays.asList(
+                        new User("Tony", 45, Arrays.asList("Red", "Green")),
+                        new User("Mark", 21, Arrays.asList("Blue", "Green", "White")),
+                        new User("Ben", 19, Arrays.asList("Green", "Violet", "Purple", "Grey")),
+                        new User("Claire", 37, Arrays.asList("White", "Black")),
+                        new User("Sarah", 49, Arrays.asList("Red", "Green", "Blue"))
+                ).stream()
+                .filter(user -> user.getAge() < 40)
+                .flatMapToInt(user -> user.getColors().stream()
+                        .mapToInt(color -> color.length()))
+                .peek(numero -> System.out.println("lenght: " + numero))
+                .reduce(0, (subTotal, element) -> subTotal + element);
+        System.out.println(reduce2);
+```
--- src/MainClass.java
+++ src/MainClass.java
@@ -10,17 +10,40 @@ public class MainClass {
 //    private static Predicate<String> isLenghtThreeChars= color-> color.length()==3;
 
     public static void main(String[] args) {
-        //sorted - Comparator
-        Arrays.asList(
+        //reduce - example 1
+        Integer reduce = Arrays.asList(1, 2, 3, 4, 5).stream()
+                .filter(integer -> integer > 2)
+                .reduce(0, (subTotal, element) -> subTotal + element);
+        System.out.println(reduce);
+
+        // example 2
+        Integer reduce1 = Arrays.asList(
                         new User("Tony", 45, Arrays.asList("Red", "Green")),
                         new User("Mark", 21, Arrays.asList("Blue", "Green", "White")),
                         new User("Ben", 19, Arrays.asList("Green", "Violet", "Purple", "Grey")),
                         new User("Claire", 37, Arrays.asList("White", "Black")),
                         new User("Sarah", 49, Arrays.asList("Red", "Green", "Blue"))
                 ).stream()
-                .sorted(Comparator.comparing(User::getFirstname))
-                .map(User::getFirstname)
-                .forEach(System.out::println);
+                .filter(user -> user.getAge() > 24)
+                .reduce(0, (subTotal, element) -> subTotal + element.getAge(), Integer::sum);
+
+        System.out.println(reduce1);
+
+        //example 3
+        int reduce2 = Arrays.asList(
+                        new User("Tony", 45, Arrays.asList("Red", "Green")),
+                        new User("Mark", 21, Arrays.asList("Blue", "Green", "White")),
+                        new User("Ben", 19, Arrays.asList("Green", "Violet", "Purple", "Grey")),
+                        new User("Claire", 37, Arrays.asList("White", "Black")),
+                        new User("Sarah", 49, Arrays.asList("Red", "Green", "Blue"))
+                ).stream()
+                .filter(user -> user.getAge() < 40)
+                .flatMapToInt(user -> user.getColors().stream()
+                        .mapToInt(color -> color.length()))
+                .peek(numero -> System.out.println("lenght: " + numero))
+                .reduce(0, (subTotal, element) -> subTotal + element);
+        System.out.println(reduce2);
+
     }
 }
 

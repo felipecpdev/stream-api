6e72d949a55bbee39fe2a7787386c151441f58c4
Felipe Contreras <felipe.contreras.dev@gmail.com>
2023-05-24 11:18:53 -0400

    map

--- README.md
+++ README.md
@@ -173,3 +173,63 @@ System.out.println(count);
 
         System.out.println(count3);
 ```
+
+## Function
+
+*clase ejemplo*
+```java
+class User {
+    private String firstname;
+    private int age;
+    private List<String> colors;
+
+    public User(String firstname, int age, List<String> colors) {
+        this.firstname = firstname;
+        this.age = age;
+        this.colors = colors;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getColors() {
+        return colors;
+    }
+
+    public void setColors(List<String> colors) {
+        this.colors = colors;
+    }
+}
+```
+
+### Map
+
+```java
+ //Map - Functional Interface example 1
+        Arrays.asList(
+                        new User("Tony", 45, Arrays.asList("Red", "Green")),
+                        new User("Mark", 21, Arrays.asList("Blue", "Green", "White")),
+                        new User("Ben", 19, Arrays.asList("Green", "Violet", "Purple", "Grey")),
+                        new User("Claire", 37, Arrays.asList("White", "Black")),
+                        new User("Sarah", 49, Arrays.asList("Red", "Green", "Blue"))
+                ).stream()
+                .filter(user -> user.getAge() > 30)
+                .peek(username -> System.out.println("user over 30: " + username.getFirstname()))
+                .map(User::getFirstname)
+                .peek(username -> System.out.println("Mapped to String: " + username))
+                .distinct()
+                .forEach(System.out::println);
+```
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
@@ -9,26 +10,57 @@ public class MainClass {
 //    private static Predicate<String> isLenghtThreeChars= color-> color.length()==3;
 
     public static void main(String[] args) {
-        //Consumer Functional Interface foreach
+        //Map - Functional Interface example 1
+        Arrays.asList(
+                        new User("Tony", 45, Arrays.asList("Red", "Green")),
+                        new User("Mark", 21, Arrays.asList("Blue", "Green", "White")),
+                        new User("Ben", 19, Arrays.asList("Green", "Violet", "Purple", "Grey")),
+                        new User("Claire", 37, Arrays.asList("White", "Black")),
+                        new User("Sarah", 49, Arrays.asList("Red", "Green", "Blue"))
+                ).stream()
+                .filter(user -> user.getAge() > 30)
+                .peek(username -> System.out.println("user over 30: " + username.getFirstname()))
+                .map(User::getFirstname)
+                .peek(username -> System.out.println("Mapped to String: " + username))
+                .distinct()
+                .forEach(System.out::println);
 
-        // example 1
-        // Arrays.asList(colors).forEach(System.out::println);
 
-        // example 2
-        AtomicInteger count = new AtomicInteger();
-        Arrays.asList(colors).stream()
-                .forEach(color -> count.addAndGet(color.length()));
-        System.out.println(count.get());
+    }
+}
 
-        //example 3 peek
-        long count3 = Arrays.asList(colors).stream()
-                .filter(color -> color.length() > 5)
-                .peek(color -> System.out.println("Filter value = " + color))
-                .map(color -> color.toUpperCase())
-                .peek(color -> System.out.println("Mapped value = " + color))
-                .count();
+class User {
+    private String firstname;
+    private int age;
+    private List<String> colors;
 
-        System.out.println(count3);
+    public User(String firstname, int age, List<String> colors) {
+        this.firstname = firstname;
+        this.age = age;
+        this.colors = colors;
+    }
 
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getColors() {
+        return colors;
+    }
+
+    public void setColors(List<String> colors) {
+        this.colors = colors;
     }
 }
